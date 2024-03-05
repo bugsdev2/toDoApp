d8b948abb6c76ea0ed26bc365380d7d6096f6918
BugsLaptop <msherlock246@gmail.com>
2024-03-05 23:41:39 +0530

    Replaced useFormik hook with formik component for better handling of forms

--- src/components/startuppage/startuppage.jsx
+++ src/components/startuppage/startuppage.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
-import { useFormik } from 'formik';
+import { useFormik, Formik, Form, ErrorMessage, Field } from 'formik';
 import { useCookies } from 'react-cookie';
 import axios from 'axios';
 import Modal from '../modal/modal.jsx';
@@ -10,34 +10,14 @@ export default function StartUpPage() {
 	const [cookie, setCookie, deleteCookie] = useCookies('userName');
 	const navigate = useNavigate();
 	const [flag, setFlag] = useState(false);
-	const transferData = {};
+	const [values, setValues] = useState({});
 
 	useEffect(() => {
 		if(cookie.userName) {
 			navigate('/dashboard');
 		}
 	}, [cookie.userName]);
-	
-	const loginForm = useFormik({
-		initialValues: {
-			userName: '',
-			password: ''
-		},
-		onSubmit: (values) => {
-			Object.assign(transferData, values);
-			setFlag(true);
-			const userName = values.userName;
-			const password = values.password;
-			axios.get(`https://todoapp-api-22b3.onrender.com/get-user/${userName}/${password}`).then((response) => {
-				setFlag(false);
-				setCookie('userName', userName, { sameSite: 'none', maxAge: 3000000 , secure: true });
-				navigate('/dashboard');
-			})
-			.catch(err => {
-				
-			})
-		}
-	});
+
 	
 	const signUpForm = useFormik({
 		initialValues: {
@@ -47,47 +27,83 @@ export default function StartUpPage() {
 			lastName: ''
 		},
 		onSubmit: (values) => {
-			setFlag(true);
-			setCookie('userName', values.userName, { sameSite: 'none', maxAge: 3000000, secure: true });
-			axios.post('https://todoapp-api-22b3.onrender.com/add-user', values).then(() => {
-				navigate('/dashboard');
-			});
+			
 		}
 	});
 	
 	const login = (
 					<div className="bg-white text-bg-purple justify-self-center rounded-3xl p-4">
 					  <div className="text-center font-bold text-lg">Login</div>
-					  <form className="grid" onSubmit={loginForm.handleSubmit}>
-						<label htmlFor="userNameLogin" className="mt-1">User Name</label>
-						<input onChange={loginForm.handleChange} id="userNameLogin" name="userName" type="text" className="border border-bg-purple rounded-xl px-3 px-1" required/>
-						<label htmlFor="passwordLogin" className="mt-1">Password</label>
-						<input onChange={loginForm.handleChange} id="passwordLogin" name="password" type="password" className="border border-bg-purple rounded-xl px-3 px-1" required/>
-						<div className="flex justify-center mt-3">
-						  <button type="submit" className="btn">Log In</button>
-						</div>
-						<div className="text-center text-sm mt-2"><Link to="/signup">Sign Up for a New Account</Link></div>
-					  </form>
+					  <Formik initialValues={ 
+							{
+								userName: '',
+								password: '',
+							}
+						  }
+						  onSubmit={(values) => {
+							setFlag(true);
+							const userName = values.userName;
+							const password = values.password;
+							axios.get(`https://todoapp-api-22b3.onrender.com/get-user/${userName}/${password}`).then((response) => {
+								if(response.data[0]) {
+									setFlag(false);
+									setCookie('userName', userName, { sameSite: 'none', maxAge: 3000000 , secure: true });
+									navigate('/dashboard');
+								} else {
+									navigate('/signup');
+									setFlag(false);
+								}
+							})
+						  }}
+						  >
+						  <Form className="grid">
+							<label htmlFor="userName" className="mt-1">User Name</label>
+							<Field id="userName" name="userName" type="text" className="border border-bg-purple rounded-xl px-3 px-1" required/>
+							<label htmlFor="password" className="mt-1">Password</label>
+							<Field id="password" name="password" type="password" className="border border-bg-purple rounded-xl px-3 px-1" required/>
+							<div className="flex justify-center mt-3">
+							  <button type="submit" className="btn">Log In</button>
+							</div>
+							<div className="text-center text-sm mt-2"><Link to="/signup">Sign Up for a New Account</Link></div>
+						  </Form>
+					  </Formik>
 					</div>
 	)
 				  
 	const signup = (
 					<div className="bg-white text-bg-purple justify-self-center rounded-3xl p-4">
 					  <div className="text-center font-bold text-lg">Sign Up</div>
-					  <form className="grid" onSubmit={signUpForm.handleSubmit}>
-						<label htmlFor="userNameSignUp" className="mt-1">User Name</label>
-						<input onChange={signUpForm.handleChange} id="userNameSignUp" name="userName" type="text" className="border border-bg-purple rounded-xl px-3 px-1" minLength="4" required/>
-						<label htmlFor="passwordSignUp" className="mt-1">Password</label>
-						<input onChange={signUpForm.handleChange} id="passwordSignUp" name="password" type="password" className="border border-bg-purple rounded-xl px-3 px-1" minLength="6" required/>
-						<label htmlFor="firstNameSignUp" className="mt-1">First Name</label>
-						<input onChange={signUpForm.handleChange} id="firstNameSignUp" name="firstName" type="text" className="border border-bg-purple rounded-xl px-3 px-1" minLength="3" required/>
-						<label htmlFor="lastNameSignUp" className="mt-1">Last Name</label>
-						<input onChange={signUpForm.handleChange} id="lastNameSignUp" name="lastName" type="text" className="border border-bg-purple rounded-xl px-3 px-1" minLength="3" required/>
-						<div className="flex justify-center mt-3">
-						  <button type="submit" className="btn">Sign Up</button>
-						</div>
-						<div className="text-center text-sm mt-2"><Link to="/login">Already have an Account? Log In</Link></div>
-					  </form>
+					  <Formik initialValues={{
+						  userName: values.userName || '',
+						  password: values.password || '',
+						  firstName: '',
+						  lastName: ''
+					  }}
+					  onSubmit = {(values) => {
+							setValues(values);
+							setFlag(true);
+							setCookie('userName', values.userName, { sameSite: 'none', maxAge: 3000000, secure: true });
+							axios.post('https://todoapp-api-22b3.onrender.com/add-user', values).then(() => {
+								navigate('/login');
+								setFlag(false);
+							});
+					  }}
+					  >
+						  <Form className="grid">
+							<label htmlFor="userName" className="mt-1">User Name</label>
+							<Field id="userName" name="userName" type="text" className="border border-bg-purple rounded-xl px-3 px-1" minLength="4" required/>
+							<label htmlFor="passwordSignUp" className="mt-1">Password</label>
+							<Field id="password" name="password" type="password" className="border border-bg-purple rounded-xl px-3 px-1" minLength="6" required/>
+							<label htmlFor="firstName" className="mt-1">First Name</label>
+							<Field id="firstName" name="firstName" type="text" className="border border-bg-purple rounded-xl px-3 px-1" minLength="3" required/>
+							<label htmlFor="lastName" className="mt-1">Last Name</label>
+							<Field id="lastName" name="lastName" type="text" className="border border-bg-purple rounded-xl px-3 px-1" minLength="3" required/>
+							<div className="flex justify-center mt-3">
+							  <button type="submit" className="btn">Sign Up</button>
+							</div>
+							<div className="text-center text-sm mt-2"><Link to="/login">Already have an Account? Log In</Link></div>
+						  </Form>
+					  </Formik>
 					</div>
 	)
 	
